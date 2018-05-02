8bdb490977ab3019412adc4ca832da4a182e24b0
Connor O'Brien <connoro@google.com>
2018-05-02 15:31:23 -0700

    sched/fair: turn off EAS_USE_NEED_IDLE sched feature
    This feature disables the standard EAS task placement behavior for
    prefer_idle tasks, causing them to prefer smaller idle CPUs instead of
    the first idle CPU they find. For now let's use the  well-understood
    EAS behavior we've used on previous devices, and revisit turning this
    feature on if we decide it's an effective way to save power.
    
    Bug: 78782859
    Test: sched stats show prefer_idle path being taken
    Change-Id: I1b7ff4865f54aa978d900cad5c3bfd69f1b71ce4
    Signed-off-by: Connor O'Brien <connoro@google.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -109,7 +109,7 @@ SCHED_FEAT(FBT_STRICT_ORDER, false)
  * ON: schedtune.prefer_idle is replaced with need_idle
  * OFF: schedtune.prefer_idle is honored as is.
  */
-SCHED_FEAT(EAS_USE_NEED_IDLE, true)
+SCHED_FEAT(EAS_USE_NEED_IDLE, false)
 
 /*
  * Apply schedtune boost hold to tasks of all sched classes.
